e7bea1d9fbbf1d5bf82dab947102db170df71321
Cranky Supertoon <48419040+CrankySupertoon@users.noreply.github.com>
2020-03-08 19:46:16 -0400

    Update Start.sh

--- Start.sh
+++ Start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-<<<<<<< HEAD
 clear
 echo = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 echo What do you want to do!
@@ -87,39 +86,3 @@ while true; do
         * );;
     esac
 done
-=======
-
-#ServerIP
-clear
-echo = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-echo What Server are you connecting to!
-echo = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-read -p "Server IP (Default: 127.0.0.1): " TTS_GAMESERVER
-TTS_GAMESERVER=${TTS_GAMESERVER:-"127.0.0.1"}
-
-#Username
-clear
-echo = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-echo Username [!] This does get stored in your source code so beware!
-echo = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-read -p "Username: " ttsUsername
-
-
-# Export the environment variables:
-export ttsUsername=$ttsUsername
-export TTS_PLAYCOOKIE=$ttsUsername
-export TTS_GAMESERVER=$TTS_GAMESERVER
-
-#Login
-clear
-echo = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-echo Welcome to Toontown Stride, $ttsUsername!
-echo The Tooniverse Awaits You!
-echo = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-
-while [ true ]
-do
-    /usr/bin/python2 -m toontown.toonbase.ToontownStart
-    read -r -p "Press any key to continue..." key
-done
->>>>>>> bf7ca321a1a42bf86a05b96e23d02cb2c80ad1ad
