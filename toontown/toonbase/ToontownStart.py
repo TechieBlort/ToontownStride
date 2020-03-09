1e821a40362fc98412189a1aadda4873c678e660
CrankySupertoon <crankysupertoon@gmail.com>
2020-03-08 20:01:47 -0400

    Remove Injector

--- toontown/toonbase/ToontownStart.py
+++ toontown/toonbase/ToontownStart.py
@@ -33,12 +33,6 @@ if not os.path.exists('user/'):
 
 
 if __debug__:
-    try:
-        import wx
-    except:
-        import wxversion
-        wxversion.select('3.0')
-        import wx
     import sys
     from direct.stdpy import threading
 
@@ -49,25 +43,6 @@ if __debug__:
         loadPrcFile('config/local.prc')
 
     defaultText = ""
-
-    def __inject_wx(_):
-        code = textbox.GetValue()
-        exec(code, globals())
-
-    def openInjector_wx():
-        app = wx.App(redirect=False)
-        frame = wx.Frame(None, title="Injector", size=(640, 400), style=wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX | wx.MINIMIZE_BOX)
-        panel = wx.Panel(frame)
-        button = wx.Button(parent=panel, id=-1, label="Inject", size=(50, 20), pos=(295, 0))
-        global textbox
-        textbox = wx.TextCtrl(parent=panel, id=-1, pos=(20, 22), size=(600, 340), style=wx.TE_MULTILINE)
-        frame.Bind(wx.EVT_BUTTON, __inject_wx, button)
-        frame.Show()
-        app.SetTopWindow(frame)
-        textbox.AppendText(defaultText)
-        threading.Thread(target=app.MainLoop).start()
-
-    openInjector_wx()
 
 from direct.directnotify.DirectNotifyGlobal import directNotify
 from direct.gui.DirectGuiGlobals import NO_FADE_SORT_INDEX, FADE_SORT_INDEX
